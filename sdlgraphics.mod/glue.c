57db827d9f77741ce1db0d38ae6f855dc0bcdc4d
woollybah <woollybah@gmail.com>
2014-09-17 09:54:44 +0100

    Fixed sending incorrect mouse button value.

--- sdlgraphics.mod/glue.c
+++ sdlgraphics.mod/glue.c
@@ -243,7 +243,7 @@ void bmx_SDL_EmitSDLEvent( SDL_Event *event, BBObject *source ) {
 			return;
 		case SDL_MOUSEBUTTONDOWN:
 		case SDL_MOUSEBUTTONUP:
-			bbSDLSystemEmitEvent( (event->type == SDL_MOUSEBUTTONDOWN) ? BBEVENT_MOUSEDOWN : BBEVENT_MOUSEUP,source,5-event->button.button,0,event->button.x,event->button.y,&bbNullObject );
+			bbSDLSystemEmitEvent( (event->type == SDL_MOUSEBUTTONDOWN) ? BBEVENT_MOUSEDOWN : BBEVENT_MOUSEUP,source,event->button.button,0,event->button.x,event->button.y,&bbNullObject );
 			return;
 		case SDL_MOUSEWHEEL:
 			bbSDLSystemEmitEvent( BBEVENT_MOUSEWHEEL,source,(event->wheel.y < 0) ? -1 : 1,0,0,0,&bbNullObject );
